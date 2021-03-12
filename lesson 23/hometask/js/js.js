b954642c36229bc92e1926a339f791f03ce090bb
Anastasija-code <nastja.91.2008@gmail.com>
2021-03-12 19:26:11 +0300

    Создала массив со своими любиммыми фильмами

---
+++ lesson 23/hometask/js/js.js
@@ -0,0 +1,5 @@
+const favouriteFilms = ["The best offer", "Pretty woman", "Green mile"];
+console.log("favouriteFilms", favouriteFilms);
+favouriteFilms[3] = "Forrest Gamp";
+favouriteFilms.push("Intouchables");
+
