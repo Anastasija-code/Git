037e6172a734f7fd66bcad0dc186e45288d5fd28
Anastasija-code <nastja.91.2008@gmail.com>
2021-03-12 19:27:59 +0300

    Создала калькулятор

--- lesson 23/hometask/js/js.js
+++ lesson 23/hometask/js/js.js
@@ -3,3 +3,26 @@ console.log("favouriteFilms", favouriteFilms);
 favouriteFilms[3] = "Forrest Gamp";
 favouriteFilms.push("Intouchables");
 
+function calc(a, b, callback) {
+    return callback(a,b) 
+ }
+ function add(a, b) {
+   return a + b;
+ }
+ function subtr(a, b) {
+   return a - b;
+ }
+ function div(a, b) {
+   return a / b;
+ }
+ function modDiv(a, b) {
+   return a % b;
+ }
+ function exp(a, b) {
+   return a ** b;
+ }
+ console.log(calc(8, 1, add, ));
+ console.log(calc(7,2, subtr));
+ console.log(calc(10,5,div));
+ console.log(calc(8,5,modDiv));
+ console.log(calc(2,2,exp));
